df93b6de032be8183f32a7ba2e472e8e9bf02fd4
Shuang Du <zealotsd@gmail.com>
2020-07-03 13:58:17 -0400

    wrapper button card in hello world app

--- hello-world-client-app/src/components/CounterCard/CounterCard.js
+++ hello-world-client-app/src/components/CounterCard/CounterCard.js
@@ -4,10 +4,12 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import ButtonToolbar from "react-bootstrap/ButtonToolbar";
 
+import WrapperButton from './WrapperButton'
+
 export default class CounterCard extends Component {
   constructor(props) {
     super(props);
-    this.state = { counter:0};
+    this.state = { counterOne:0, counterTwo:0};
 
   }
 
@@ -15,18 +17,14 @@ export default class CounterCard extends Component {
     document.title = "CounterCard";
   }
 
-  addHandler = () => {
-    const count = this.state.counter
-    this.setState({
-      counter: count + 1
-    })
+  addHandler = (cardNum = 1) => {
+    if (cardNum === 1) this.setState({counterOne: this.state.counterOne + 1})
+    if (cardNum === 2) this.setState({counterTwo: this.state.counterTwo + 1})
   }
 
-  subtractHandler = () => {
-    const count = this.state.counter
-    this.setState({
-      counter: count - 1
-    })
+  subtractHandler = (cardNum = 1) => {
+    if (cardNum === 1) this.setState({counterOne: this.state.counterOne - 1})
+    if (cardNum === 2) this.setState({counterTwo: this.state.counterTwo - 1})
   }
 
   render() {
@@ -38,15 +36,31 @@ export default class CounterCard extends Component {
           <Card.Body>
             <Card.Title>Counter Card</Card.Title>
             <Card.Text>
-              This is the count {this.state.counter}
+              This is the count {this.state.counterOne}
             </Card.Text>
             <ButtonToolbar style={{justifyContent: 'center'}}>
-              <Button variant="primary" size='lg' onClick = {this.addHandler}>Add</Button>
-              <Button variant="secondary" size='lg' onClick = {this.subtractHandler}>Subtract</Button>
+              <Button variant="primary" size='lg' onClick = {() => this.addHandler(1)}>Add</Button>
+              <Button variant="secondary" size='lg' onClick = {() => this.subtractHandler(1)}>Subtract</Button>
               
             </ButtonToolbar>  
-            <div onClick = {this.addHandler}>Add in a div</div> 
-            <div onClick = {this.subtractHandler}>Subtract in a div</div>
+            <div onClick = {() => this.addHandler(1)}>Add in a div</div> 
+            <div onClick = {() => this.subtractHandler(1)}>Subtract in a div</div>
+          </Card.Body>
+        </Card>
+        <br/>
+        <Card style={{ width: '18rem' }}>
+          <Card.Body>
+            <Card.Title>Counter Card With Wrapped Component</Card.Title>
+            <Card.Text>
+              This is the count {this.state.counterTwo}
+            </Card.Text>
+            <ButtonToolbar style={{justifyContent: 'center'}}>
+              <WrapperButton variant="primary" size='lg' onClick = {() => this.addHandler(2)}>Add</WrapperButton>
+              <WrapperButton variant="secondary" size='lg' onClick = {() => this.addHandler(2)}>Subtract</WrapperButton>
+              
+            </ButtonToolbar>  
+            <div onClick = {() => this.addHandler(2)}>Add in a div</div> 
+            <div onClick = {() => this.subtractHandler(2)}>Subtract in a div</div>
           </Card.Body>
         </Card>
       </>
